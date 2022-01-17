2ac891c8aa64a5bdd5d197407ddf9b25344209de
Ankur Shahi <shahiankur80@gmail.com>
2022-01-16 21:03:55 -0500

    replicating sever data set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import {BiCalendar} from "react-icons/bi"
 import AddAppointment from "./components/AddAppointment";
 import Search from "./components/Search";
-import appointmentList from "./data.json";
 import AppointmentInfo from "./components/AppointmentInfo";
+import{useState, useEffect, useCallback} from 'react';
 
 function App() {
+
+  let[appointmentList, setAppointmentList]=useState([]);
+
+  const fetchData = useCallback(()=>{
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data=>{
+        setAppointmentList(data)  
+      });
+  },[])
+
+  useEffect(()=>{
+    fetchData()
+  },[fetchData]);
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3"><BiCalendar className="inline-block text-red-400 align-top"/>Your Appointments</h1>
