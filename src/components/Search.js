4776db92b09e01dab37bad98554bd31338829b3d
Ankur Shahi <shahiankur80@gmail.com>
2022-01-19 00:11:55 -0500

    sort function completed

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,29 +10,29 @@ const DropDown =({toggleSort, sortBy, onSortByChange, orderBy, onOrderByChange})
       <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
         <div onClick={()=>onSortByChange('petName')} 
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer" role="menuitem">
-          Pet Name<BiCheck/>
+          Pet Name{(sortBy==='petName')&& <BiCheck/>}
         </div>
         <div onClick={()=>onSortByChange('ownerName')}  
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer" role="menuitem">
-          Owner Name<BiCheck/>
+          Owner Name{(sortBy==='ownerName')&& <BiCheck/>}
         </div>
-        <div onClick={()=>onSortByChange('date')}  
+        <div onClick={()=>onSortByChange('aptDate')}  
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer" role="menuitem">
-          Date<BiCheck/>
+          Date{(sortBy==='aptDate')&& <BiCheck/>}
         </div>
         <div onClick={()=>onOrderByChange('asc')}  
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2" role="menuitem">
-          Asc<BiCheck/>
+          Asc{(orderBy==='asc')&& <BiCheck/>}
         </div>
         <div onClick={()=>onOrderByChange('desc')} 
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer" role="menuitem">
-          Desc<BiCheck/>
+          Desc{(orderBy==='desc')&& <BiCheck/>}
         </div>
       </div>
     </div>
   );
 }
-const Search = ({query, onQueryChange, sortBy, onSortbyChange, orderBy, onOrderByChange})=>{
+const Search = ({query, onQueryChange, sortBy, onSortByChange, orderBy, onOrderByChange})=>{
   let[toggleSort, setToggleSort]=useState(false);
   return(
     <div className="py-5">
